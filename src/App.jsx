1ef75e3ede104ca40b1bbdb6c83255247bb0d3fd
wildanzr <graita.sukma@gmail.com>
2023-03-09 23:05:37 +0700

    feat: init fresh vite app with tailwindcss and eslint

---
+++ src/App.jsx
@@ -0,0 +1,9 @@
+import React from 'react'
+
+export default function App () {
+  return (
+    <div className='w-full flex flex-col items-center justify-center'>
+      <p className='text-4xl font-bold text-blue-500'>Hello World</p>
+    </div>
+  )
+}
